bc6c4c79c3f8655e31d76558904fb20a086ffaf1
WM <wsuan15@gmail.com>
2023-07-19 17:21:43 +0800

    setup django half way

---
+++ setup6_django.sh
@@ -0,0 +1,18 @@
+# Activate the virtual environment
+pip install django
+pip install djangorestframework
+pip install django-rest-knox
+pip install --upgrade pip
+
+
+python -m django --version
+django-admin startproject backend_robot
+cd backend_robot
+python manage.py runserver
+python manage.py makemigrations
+python manage.py migrate
+python manage.py showmigrations
+
+python manage.py startapp test_app
+
+pip install djangorestframework django-rest-knox django-cors-headers daphne channels
