d7fbe21cf0898c84a15bc8a88f4c2c32f05f2188
WM <wsuan15@gmail.com>
2023-07-20 18:37:54 +0800

    now need do ssl for camera permission

--- setup7_redis.sh
+++ setup7_redis.sh
@@ -6,3 +6,9 @@ apt-get update
 apt-get install redis
 service redis-server start
 redis-cli
+
+
+
+######SSL###############
+curl -O -k "https://getcert:zdf76tde7bg7bfs@103.68.63.123/ssl/kynoci.com-sub-privkey.pem"
+curl -O -k "https://getcert:zdf76tde7bg7bfs@103.68.63.123/ssl/kynoci.com-sub-cert.pem"
