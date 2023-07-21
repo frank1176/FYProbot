3308cf356829cfcc7b64b8356586b8051c96d09c
WM <wsuan15@gmail.com>
2023-07-21 09:23:30 +0100

    update

--- chromium-browser/index.js
+++ chromium-browser/index.js
@@ -1,10 +1,10 @@
     const puppeteer = require('puppeteer-core');
 
-    // function delay(time) {
-    //     return new Promise(function(resolve) { 
-    //         setTimeout(resolve, time)
-    //     });
-    // }
+    function delay(time) {
+        return new Promise(function(resolve) { 
+            setTimeout(resolve, time)
+        });
+    }
 
     (async () => {
         const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser', 
@@ -28,7 +28,7 @@
         // const answerBtn = await page.$('#answerButton');
         // answerBtn.click();
         // await delay(5000);
-
+        await delay(5000)
         await page.screenshot({path: 'screeshot3.png'});
         // await browser.close();
     })();
